ab7f7d37ec44ca32d3b30854b5d6e12e057d5fcf
Shota Hayashi <qsezy1@hotmail.com>
2017-12-09 19:44:08 +0900

    タブを指定するキーの順番を修正

--- loadTabs.js
+++ loadTabs.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   chrome.tabs.query({}, function(tabs){
 
-    var keies = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    var keies = '1234567890asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM';
     var windowIds = [];
     var keyAssigns = [];
     var elements = '';
